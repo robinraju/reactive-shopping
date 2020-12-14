5369166611d96afa8ff3fdea6effa1d54de2efdd
Robin Raju <robinraju@users.noreply.github.com>
2020-12-14 23:30:56 +0100

    Implement  shopping cart service with ability to send messages to sharded entities

--- shopping-cart-service/src/main/scala/shopping/cart/Main.scala
+++ shopping-cart-service/src/main/scala/shopping/cart/Main.scala
@@ -33,7 +33,7 @@ class Main(context: ActorContext[Nothing])
   val grpcPort =
     system.settings.config.getInt("shopping-cart-service.grpc.port")
 
-  val grpcService = new ShoppingCartServiceImpl
+  val grpcService = new ShoppingCartServiceImpl(system)
 
   ShoppingCartServer.start(grpcInterface, grpcPort, system, grpcService)
 
--- shopping-cart-service/src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
+++ shopping-cart-service/src/main/scala/shopping/cart/ShoppingCartServiceImpl.scala
@@ -1,17 +1,53 @@
 package shopping.cart
+import akka.actor.typed.ActorSystem
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+import akka.grpc.GrpcServiceException
+import akka.util.Timeout
+import io.grpc.Status
 import org.slf4j.LoggerFactory
 import shopping.cart.proto.{ AddItemRequest, Cart }
 
-import scala.concurrent.Future
+import scala.concurrent.{ Future, TimeoutException }
 
-class ShoppingCartServiceImpl extends proto.ShoppingCartService {
+class ShoppingCartServiceImpl(system: ActorSystem[_])
+    extends proto.ShoppingCartService {
+
+  import system.executionContext
 
   private val logger = LoggerFactory.getLogger(getClass)
+
+  implicit private val timeout: Timeout =
+    Timeout.create(
+      system.settings.config.getDuration("shopping-cart-service.ask-timeout"))
+
+  private val sharding = ClusterSharding(system)
 
   override def addItem(in: AddItemRequest): Future[Cart] = {
     logger.info("Add item {} to cart {}", in.itemId, in.cartId)
 
-    Future.successful(
-      proto.Cart(items = List(proto.Item(in.itemId, in.quantity))))
+    val entityRef = sharding.entityRefFor(ShoppingCart.EntityKey, in.cartId)
+    val reply: Future[ShoppingCart.Summary] =
+      entityRef.askWithStatus(ShoppingCart.AddItem(in.itemId, in.quantity, _))
+    val response = reply.map(cart => toProtoCart(cart))
+    convertError(response)
+  }
+
+  private def toProtoCart(cart: ShoppingCart.Summary): proto.Cart = {
+    proto.Cart(cart.items.iterator.map { case (itemId, quantity) =>
+      proto.Item(itemId, quantity)
+    }.toSeq)
+  }
+
+  private def convertError[T](response: Future[T]): Future[T] = {
+    response.recoverWith {
+      case _: TimeoutException =>
+        Future.failed(
+          new GrpcServiceException(
+            Status.UNAVAILABLE.withDescription("Operation timed out")))
+      case exc =>
+        Future.failed(
+          new GrpcServiceException(
+            Status.INVALID_ARGUMENT.withDescription(exc.getMessage)))
+    }
   }
 }
