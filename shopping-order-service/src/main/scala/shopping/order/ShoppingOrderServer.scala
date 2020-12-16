fc6fdc95a5baeaf8a15c0262951efa10677cf3a4
Robin Raju <robinraju@users.noreply.github.com>
2020-12-16 20:54:04 +0100

    Implement shopping order gRPC server

---
+++ shopping-order-service/src/main/scala/shopping/order/ShoppingOrderServer.scala
@@ -0,0 +1,47 @@
+package shopping.order
+
+import akka.actor.typed.ActorSystem
+import akka.grpc.scaladsl.{ ServerReflection, ServiceHandler }
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ HttpRequest, HttpResponse }
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.{ Failure, Success }
+
+object ShoppingOrderServer {
+  def start(
+      interface: String,
+      port: Int,
+      system: ActorSystem[_],
+      grpcService: proto.ShoppingOrderService): Unit = {
+    implicit val sys: ActorSystem[_] = system
+    implicit val ec: ExecutionContext =
+      system.executionContext
+
+    val service: HttpRequest => Future[HttpResponse] =
+      ServiceHandler.concatOrNotFound(
+        proto.ShoppingOrderServiceHandler.partial(grpcService),
+        // ServerReflection enabled to support grpcurl without import-path and proto parameters
+        ServerReflection.partial(List(proto.ShoppingOrderService)))
+
+    val bound =
+      Http()
+        .newServerAt(interface, port)
+        .bind(service)
+        .map(_.addToCoordinatedShutdown(3.seconds))
+
+    bound.onComplete {
+      case Success(binding) =>
+        val address = binding.localAddress
+        system.log.info(
+          "Shopping order at gRPC server {}:{}",
+          address.getHostString,
+          address.getPort)
+      case Failure(ex) =>
+        system.log.error("Failed to bind gRPC endpoint, terminating system", ex)
+        system.terminate()
+    }
+  }
+
+}
